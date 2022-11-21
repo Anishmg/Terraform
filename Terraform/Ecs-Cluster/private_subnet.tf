68135fc1f9dc5b4d6d9adbfa98ab7a4cc50cdaf4
Anishmg <111762944+Anishmg@users.noreply.github.com>
2022-11-21 11:39:56 +0530

    Add files via upload

---
+++ Terraform/Ecs-Cluster/private_subnet.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.aws-vpc.id
+  count             = length(var.private_subnets)
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name        = "${var.app_name}-private-subnet-${count.index + 1}"
+    Environment = var.app_environment
+  }
+}
---
+++ Terraform/Ecs-Cluster/terraform.tfvars
@@ -0,0 +1,10 @@
+aws_region     = "us-east-1"
+aws_access_key = "AKIA4BWTYS66JLQGOBNR"
+aws_secret_key = "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"
+
+availability_zones = ["us-east-1a", "us-east-1b"]
+public_subnets     = ["10.10.100.0/24", "10.10.101.0/24"]
+private_subnets    = ["10.10.0.0/24", "10.10.1.0/24"]
+
+app_name        = "Terraform"
+app_environment = "production"
